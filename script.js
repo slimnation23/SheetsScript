c91561eff72028203c549b69f704430637b9e37c
slimnation23 <svetaberynda@gmail.com>
2023-04-14 22:53:53 +0200

    4

--- script.js
+++ script.js
@@ -38,14 +38,18 @@
 //     return parent.appendChild(el)
 // }
 
-const url =
-    'https://script.google.com/macros/s/AKfycbyKpAVfTF0e_E9KhrKhCWNdGOCOcgJjgm0aVDK9Seoim96cmldc7-tRT6Vkx2JMObFclA/exec'
+const url = 'https://script.google.com/macros/s/AKfycbyKpAVfTF0e_E9KhrKhCWNdGOCOcgJjgm0aVDK9Seoim96cmldc7-tRT6Vkx2JMObFclA/exec'
 
 const btn = document.querySelector('.btn')
 const output = document.querySelector('.output')
 btn.textContent = 'Start Game'
 btn.onclick = startGame
-const game = { data: {}, que: 0, score: 0 }
+const game = {
+    data: {},
+    que: 0,
+    score: 0,
+    ans: []
+}
 
 function startGame() {
     btn.style.display = 'none'
@@ -83,6 +87,7 @@ function loadQuestion() {
 
 function checker(e) {
     const el = e.target
+    game.ans.push(el.selOpt)
     const boxs = document.querySelectorAll('.box')
     boxs.forEach((ele) => {
         ele.removeEventListener('click', checker)
@@ -102,9 +107,29 @@ function checker(e) {
     }
     const btn1 = maker('button', output, 'btn1', `Next Question`)
     game.que++
-    console.log(`remaining ${game.data.length-game.que}`);
-    btn1.onclick = loadQuestion
+    const total = game.data.length-game.que
+    console.log(`remaining ${total}`)
+    if (total == 0) {
+        btn.textContent = 'Game over See Score'
+        btn1.onclick = endGame
+    } else {
+        btn1.onclick = loadQuestion
+    }
     console.log(el.correct, el.selOpt);
+}
+
+function endGame() {
+    // output.textContent = JSON.stringify(game)
+    output.innerHTML = ''
+    game.ans.forEach((ele, ind) => {
+        let html = `Q:${game.data[ind].question} C:${game.data[ind].answer} R:${ele}`
+        const div = maker('div', output, 'main', html)
+        const bg = (game.data[ind].answer == ele) ? 'green' : 'red'
+        div.style.color = bg
+    })
+    const htmlScore = `${game.score} out of ${game.data.length}`
+    const div = maker('div', output, 'main', htmlScore)
+    console.log(game);
 }
 
 function maker(t, p, c, h) {
