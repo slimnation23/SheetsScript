60f5cedaed92ff1b7c2c82fb6c21367ec210e57d
slimnation23 <svetaberynda@gmail.com>
2023-04-16 17:32:59 +0200

    7

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const url = 'https://script.google.com/macros/s/AKfycbzG9TqrOgyIRdqC0SgsJqZaHPKlgCf78XUWP87TQxOwTepB1ZY92km_EAfr6rn3zX2v8Q/exec'
+const url =
+    'https://script.google.com/macros/s/AKfycbwknELcF4sr20LFxGT1FRnpWHY9oy0b3OxzmiMzaebWMInLGspok-pQCXU66-lLWm-M_Q/exec'
 const btn = document.querySelector('.btn')
 const output = document.querySelector('.output')
 btn.textContent = 'Start Game'
@@ -7,30 +8,19 @@ const game = {
     data: {},
     que: 0,
     score: 0,
-    ans: []
+    ans: [],
 }
 
 function startGame() {
     btn.style.display = 'none'
-    output.textContent = 'Starting game, loading data...'   
-    btn.textContent = 'Get Sheet data'
-    btn.onclick = (e) => {
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => {
-                game.data = data
-                buildGame()
-            })
-            .then(rep => rep.json())
-            .then(data => {
-                console.log(data)
-                const headings = data[0]
-                const rows = data.slice(1)
-                outputData(headings, rows)
-            })
-    }
+    output.textContent = 'Starting game, loading data...'
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            game.data = data
+            buildGame()
+        })
 }
-    
 
 function buildGame() {
     console.log(game.data)
@@ -45,25 +35,12 @@ function loadQuestion() {
     q.opts.sort(() => {
         return Math.random() - 0.5
     })
-    const tabl = maker('table', output, 'table')
-    const heading = maker('tr', tabl, 'row')
-    headings.forEach(ele => {
-        const el = maker('th', heading, 'th')
-        el.textContent = ele
-    })
     q.opts.forEach((el) => {
         const span = maker('span', main, 'box', el)
         span.correct = q.answer
         span.selOpt = el
         span.classList.add('box1')
         span.addEventListener('click', checker)
-    })
-    rows.forEach(row => {
-        const r = maker('tr', tabl, 'row')
-        row.forEach(col => {
-            const el = maker('td', r, 'td')
-            el.textContent = col
-        })
     })
     console.log(q)
 }
@@ -78,19 +55,24 @@ function checker(e) {
         ele.classList.remove('box1')
         ele.disabled = true
     })
-    if(el.correct == el.selOpt) {
+    if (el.correct == el.selOpt) {
         game.score++
         const main = maker('div', output, 'main', 'Correct answer')
         el.style.color = 'white'
         el.style.backgroundColor = 'green'
     } else {
-        const main = maker('div', output, 'main', `Wrong answer was ${el.correct}`)
+        const main = maker(
+            'div',
+            output,
+            'main',
+            `Wrong answer was ${el.correct}`
+        )
         el.style.color = 'white'
         el.style.backgroundColor = 'red'
     }
-    const btn1 = maker('button', output, 'btn1', `Next Question`)
+    const btn1 = maker('button', output, 'btn1', 'Next Question')
     game.que++
-    const total = game.data.length-game.que
+    const total = game.data.length - game.que
     console.log(`remaining ${total}`)
     if (total == 0) {
         btn.textContent = 'Game over See Score'
@@ -98,13 +80,7 @@ function checker(e) {
     } else {
         btn1.onclick = loadQuestion
     }
-    console.log(el.correct, el.selOpt);
-}
-    
-function maker(t, parent, c) {
-    const el = document.createElement(t)
-    el.classList.add(c)
-    return parent.appendChild(el)
+    console.log(el.correct, el.selOpt)
 }
 
 function endGame() {
@@ -113,39 +89,44 @@ function endGame() {
     game.ans.forEach((ele, ind) => {
         let html = `Q:${game.data[ind].question} C:${game.data[ind].answer} R:${ele}`
         const div = maker('div', output, 'main', html)
-        const bg = (game.data[ind].answer == ele) ? 'green' : 'red'
+        const bg = game.data[ind].answer == ele ? 'green' : 'red'
         div.style.color = bg
     })
+    const htmlScore = `Your final score: ${game.score} correct out of ${game.data.length} questions`
+    const div = maker('div', output, 'main', htmlScore)
+    div.style.fontSize = '1.5em'
+    const userIn = maker('div', output, 'main', '')
+    const myInput1 = document.createElement('input')
+    myInput1.setAttribute('type', 'text')
+    const label1 = document.createTextNode('Name:')
+    userIn.append(label1)
+    myInput1.value = 'Yavorskyi Oleksandr'
+    userIn.append(myInput1)
+    const btnSend = maker('button', userIn, 'btn1', 'Send score')
+    btnSend.onclick = () => {
+        sendData(myInput1.value, userIn)
+    }
+    console.log(game)
 }
 
-
-
-
-
-const myInput1 = document.createElement('input')
-const myInput2 = document.createElement('input')
-const label1 = document.createTextNode('Name:')
-const label2 = document.createTextNode('Score:')
-myInput1.setAttribute('type', 'text')
-myInput2.setAttribute('type', 'number')
-myInput1.value = 'Yavorskyi Oleksandr'
-myInput2.value = 1000
-output.append(label1)
-output.append(myInput1)
-output.append(label2)
-output.append(myInput2)
-btn.onclick = sendData
-
-function sendData() {
+function sendData(valName, ele) {
     const formData = new FormData()
-    formData.append('name', myInput1.value)
-    formData.append('score', myInput2.value)
+    formData.append('name', valName)
+    formData.append('score', game.score)
+    formData.append('total', game.data.length)
     fetch(url, {
-        method : 'POST',
-        body : formData
-    }).then(rep => rep.json()).then(data => {
-        console.log(data);
+        method: 'POST',
+        body: formData,
     })
+        .then((rep) => rep.json())
+        .then((data) => {
+            console.log(data)
+            ele.innerHTML = `Score Submited, Thank You`
+        })
 }
 
-
+function maker(t, parent, c) {
+    const el = document.createElement(t)
+    el.classList.add(c)
+    return parent.appendChild(el)
+}
